7e91c4dbc17e873f5fb2c2c57de9d75931d42c4b
nicolebzf <57570357+nicolebzf@users.noreply.github.com>
2021-05-11 16:44:35 -0400

    Add files via upload
    text processor

--- text_processing.py
+++ text_processing.py
@@ -19,58 +19,39 @@ class TextProcessing:
 
     @classmethod
     def from_nltk(
-        cls,
-        stemmer: Any = PorterStemmer().stem,
-        stop_words: List[str] = stopwords.words("english"),
+            cls, stemmer: Any = PorterStemmer().stem, stop_words=None
     ) -> "TextProcessing":
-        """
-        initialize from nltk
-        :param stemmer:
-        :param stop_words:
-        :return:
-        """
-        return cls(stemmer, set(stop_words))
+        if stop_words is None:
+            stop_words = set(stopwords.words("english"))
+        return cls(stemmer, stop_words)
 
-    def normalize(self, token: str) -> str:
-        """
-        normalize the token based on:
-        1. make all characters in the token to lower case
-        2. remove any characters from the token other than alphanumeric characters and dash ("-")
-        3. after step 1, if the processed token appears in the stop words list or its length is 1, return an empty string
-        4. after step 1, if the processed token is NOT in the stop words list and its length is greater than 1, return the stem of the token
-        :param token:
-        :return:
-        """
-        token = token.lower()
-        token = re.sub(r'[^A-Za-z0-9 -]', '', token)
-        if len(token) == 1 or (token in self.STOP_WORDS):
+    def is_stop_words(self, token: str) -> bool:
+        return token in self.STOP_WORDS
+
+    def is_valid(self, token: str) -> bool:
+        return len(token) > 1 and (not self.is_stop_words(token))
+
+    def normalize(self, token: str, use_stemmer: bool) -> str:
+        normalized = re.sub(r"[^a-zA-Z0-9\-]", "", token.lower())
+        if self.is_valid(normalized):
+            if use_stemmer:
+                return self.stemmer(normalized)
+            else:
+                return normalized
+        else:
             return ""
-        return self.stemmer(token)
 
-    def get_normalized_tokens(self, title: str, content: str) -> List[str]:
-        """
-        pass in the title and content_str of each document, and return a list of normalized tokens (exclude the empty string)
-        you may want to apply word_tokenize first to get un-normalized tokens first.
-        Note that you don't want to remove duplicate tokens as what you did in HW3, because it will later be used to compute term frequency
-        :param title:
-        :param content:
-        :return:
-        """
-        if title != None:
-            titlelist = word_tokenize(title)
-        else:
-            titlelist = [""]
-        if content != None:
-            contentlist = word_tokenize(content)
-        else:
-            contentlist = [""]
-        contentlist = contentlist.extend(titlelist)
-        normalizedtokens = []
-        for i in range(len(contentlist)):
-            token = (self.normalize(contentlist[i]))
-            if token != "":
-                normalizedtokens.append(token)
-        return normalizedtokens
+    def get_valid_tokens(
+            self, title: str, content: str, *, use_stemmer: bool = True
+    ) -> List[str]:
+        tokens = word_tokenize(content.lower()) + title.lower().split()
+        normalized = []
+        for tok in tokens:
+            normalized_tok = self.normalize(tok, use_stemmer)
+            if normalized_tok:
+                normalized.append(normalized_tok)
+
+        return normalized
 
     @staticmethod
     def idf(N: int, df: int) -> float:
